cc5deebbcecc955edfa504ff7cabd4b00f5c5b3c
Prabhat Sharma <116380824+prabhatsharma9438@users.noreply.github.com>
2024-05-14 20:18:39 +0530

    Add files via upload

---
+++ city.js
@@ -0,0 +1,28 @@
+const countryList = {
+  Madhya_Pradesh_Cities: [
+    "All",
+"Annapurna Road",
+"Bengali Square",
+"Bhawarkua",
+"Geeta Bhawan",
+"Khandwa Road",
+"Khajrana",
+"Lokmanya Nagar",
+"MG Road (Mahatma Gandhi Road)",
+"Mushakhedi",
+"Nanda Nagar",
+"Navlakha",
+"Old Palasia",
+"Palasia",
+"Pipliyahana",
+"Piplya Kumar",
+"Rajendra Nagar",
+"Rau",
+"Sudama Nagar",
+"Tilak Nagar",
+"Vijay Nagar"
+  ]
+};
+
+/*
+*/
